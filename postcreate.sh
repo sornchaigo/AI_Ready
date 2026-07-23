d84ab97e7a7fa7c31177cbb9d8eeddaad9a2cf45
Sornchai Udomthanapong <sornchai.ud@go.buu.ac.th>
2026-07-23 09:01:42 +0000

    fix postcreate.sh

--- postcreate.sh
+++ postcreate.sh
@@ -1,7 +1,8 @@
 sudo chown -R ${UID}:${GID} ${HOME} /workspaces
 
-uv venv --prompt .venv 
+uv venv --clear
 uv sync
+source /workspaces/.venv/bin/activate
 
 
 uvicorn aiready.asgi:application --reload --port 8000
--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 # uvicorn app.main:app --reload --port 8080 
+uv venv --clear
+uv sync
+source /workspaces/.venv/bin/activate
+
 uvicorn aiready.asgi:application --reload --port 8000
