0372b65b85fede2da4f1cfb022e588cf0d54e965
Sornchai Udomthanapong <sornchai.ud@go.buu.ac.th>
2026-07-24 04:40:30 +0000

    Fix post create to run

--- postcreate.sh
+++ postcreate.sh
@@ -3,5 +3,5 @@ sudo chown -R ${UID}:${GID} ${HOME} /workspaces
 uv venv --clear
 uv sync
 source /workspaces/.venv/bin/activate
-
+cd AI_Ready
 uvicorn aiready.asgi:application --reload --port 8000
--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ uv venv --clear
 uv sync
 source /workspaces/.venv/bin/activate
 
-uv run manage.py collectstatic
+# uv run manage.py collectstatic
 uv run manage.py migrate --no-input
 # uvicorn aiready.asgi:application --reload --port 8000
