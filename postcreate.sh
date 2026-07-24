93c66cb0c9852013b7992ca4aebb3bb5e3945264
Sornchai Udomthanapong <sornchai.ud@go.buu.ac.th>
2026-07-24 04:54:29 +0000

    Fix uvicorn in postcreate

--- postcreate.sh
+++ postcreate.sh
@@ -1,7 +1,8 @@
 sudo chown -R ${UID}:${GID} ${HOME} /workspaces
+source /workspaces/.venv/bin/activate
 
+cd AI_Ready
 uv venv --clear
 uv sync
-source /workspaces/.venv/bin/activate
-cd AI_Ready
+
 uvicorn aiready.asgi:application --reload --port 8000
